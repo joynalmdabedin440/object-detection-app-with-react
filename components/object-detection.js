25d33c7c9239500f8ae97a62fdef2123ffaf9c00
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-11-17 00:42:06 +0600

    add webcam

--- components/object-detection.js
+++ components/object-detection.js
@@ -1,7 +1,18 @@
+"use client"
 import React from 'react'
+import Webcam from 'react-webcam'
 
 export const ObjectDetection = () => {
   return (
-    <div>object-detection</div>
+      <div className='mt-8'>
+          <div className='relative flex justify-center items-center gradient p-1.5 rounded-md gradient'>
+              {/* webcame */}
+              
+              <Webcam className=" rounded-md w-full lg:h-[720px]" muted/>
+                  
+              
+          </div>
+          
+    </div>
   )
 }
