dcaebdad4197d328af0637ee36af660e165c299a
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-11-17 00:22:24 +0600

    add object detection components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { ObjectDetection } from "../../components/object-detection";
+
 
 export default function Home() {
   return (
-    <div className="text-cyan-400">
-      hello
-    </div>
+    <main className=" flex min-h-screen flex-col items-center p-8">
+      <h1 className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">Object Detection</h1>
+      <ObjectDetection />
+   </main>
   );
 }
