36a45cc05d386ac6082b00c597878a9a7ba4122f
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-11-18 01:43:14 +0600

    install and setup cocossd tensorflow model

--- components/object-detection.js
+++ components/object-detection.js
@@ -1,20 +1,36 @@
 "use client"
-import React, { use, useRef } from 'react'
+import React, { use, useEffect, useRef, useState } from 'react'
 import Webcam from 'react-webcam'
+import {load as cocoSSDLoad } from '@tensorflow-models/coco-ssd'
+
 
 export const ObjectDetection = () => {
   const webcamRef = useRef(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const runCOcoSSd = async () => {
+    setIsLoading(true)
+    const net = await cocoSSDLoad()
+    setIsLoading(false)
+  }
 
   const showMyVideo = () => {
     if (webcamRef.current !== null && webcamRef.current.video?.readyState === 4) {
       const myVideoWidth = webcamRef.current.video.videoWidth 
       const myVideoHeight = webcamRef.current.video.videoHeight
-      console.log("Video dimensions: ", myVideoWidth, myVideoHeight);
+
+      webcamRef.current.video.width = myVideoWidth
+      webcamRef.current.video.height = myVideoHeight
+      
 
       
     }
     
   } 
+
+  useEffect(() => {
+    showMyVideo()
+  },[])
   
   return (
       <div className='mt-8'>
