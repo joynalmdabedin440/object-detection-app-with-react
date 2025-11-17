8c55606ba9b36ca9aa954b08ce5f392821571cbd
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-11-17 20:01:46 +0600

    add showMyVideo function

--- components/object-detection.js
+++ components/object-detection.js
@@ -1,14 +1,27 @@
 "use client"
-import React from 'react'
+import React, { use, useRef } from 'react'
 import Webcam from 'react-webcam'
 
 export const ObjectDetection = () => {
+  const webcamRef = useRef(null)
+
+  const showMyVideo = () => {
+    if (webcamRef.current !== null && webcamRef.current.video?.readyState === 4) {
+      const myVideoWidth = webcamRef.current.video.videoWidth 
+      const myVideoHeight = webcamRef.current.video.videoHeight
+      console.log("Video dimensions: ", myVideoWidth, myVideoHeight);
+
+      
+    }
+    
+  } 
+  
   return (
       <div className='mt-8'>
           <div className='relative flex justify-center items-center gradient p-1.5 rounded-md gradient'>
               {/* webcame */}
               
-              <Webcam className=" rounded-md w-full lg:h-[720px]" muted/>
+              <Webcam ref={webcamRef} className=" rounded-md w-full lg:h-[720px]" muted/>
                   
               
           </div>
