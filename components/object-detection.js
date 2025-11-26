0483f356f5ac61c8d8bc20e1647ed3cd363b8e70
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-11-26 22:11:03 +0600

    add canvasRef and logic

--- components/object-detection.js
+++ components/object-detection.js
@@ -1,47 +1,76 @@
 "use client"
 import React, { use, useEffect, useRef, useState } from 'react'
 import Webcam from 'react-webcam'
-import {load as cocoSSDLoad } from '@tensorflow-models/coco-ssd'
+import { load as cocoSSDLoad } from '@tensorflow-models/coco-ssd'
+import * as tf from '@tensorflow/tfjs'
 
-
+let detectInterval;
 export const ObjectDetection = () => {
   const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
   const [isLoading, setIsLoading] = useState(false)
 
   const runCOcoSSd = async () => {
     setIsLoading(true)
     const net = await cocoSSDLoad()
     setIsLoading(false)
+    detectInterval = setInterval(() => {
+      runObjectDetection(net)
+    }, 10)
   }
 
-  const showMyVideo = () => {
-    if (webcamRef.current !== null && webcamRef.current.video?.readyState === 4) {
-      const myVideoWidth = webcamRef.current.video.videoWidth 
-      const myVideoHeight = webcamRef.current.video.videoHeight
+  const runObjectDetection = async (net) => {
+    if (canvasRef.current && webcamRef.current && webcamRef.current.video.readyState === 4) {
+      // Get Video Properties
+      canvasRef.current.width = webcamRef.current.video.videoWidth
+      canvasRef.current.height = webcamRef.current.video.videoHeight
 
-      webcamRef.current.video.width = myVideoWidth
-      webcamRef.current.video.height = myVideoHeight
+      const detectedObjects = await net.detect(webcamRef.current.video, undefined, 0.7)
       
-
+      console.log(detectedObjects);
       
     }
-    
-  } 
+  }
 
-  useEffect(() => {
-    showMyVideo()
-  },[])
+    const showMyVideo = () => {
+      if (webcamRef.current !== null && webcamRef.current.video?.readyState === 4) {
+        const myVideoWidth = webcamRef.current.video.videoWidth
+        const myVideoHeight = webcamRef.current.video.videoHeight
+
+        webcamRef.current.video.width = myVideoWidth
+        webcamRef.current.video.height = myVideoHeight
+      
+
+      
+      }
+    
+    }
+
+    useEffect(() => {
+      runCOcoSSd()
+      showMyVideo()
+    
+    }, [])
   
-  return (
+    return (
       <div className='mt-8'>
-          <div className='relative flex justify-center items-center gradient p-1.5 rounded-md gradient'>
-              {/* webcame */}
+        {
+          isLoading ? <div>Loading model...</div> : <div className='relative flex justify-center items-center gradient p-1.5 rounded-md gradient'>
+            {/* webcame */}
               
-              <Webcam ref={webcamRef} className=" rounded-md w-full lg:h-[720px]" muted/>
+            <Webcam ref={webcamRef} className=" rounded-md w-full lg:h-[720px]" muted />
+          
+            {/* canvas */}
+            <canvas
+              ref={canvasRef}
+              className="absolute rounded-md w-full lg:h-[720px]"
+            />
                   
               
           </div>
+        }
           
-    </div>
-  )
-}
+      </div>
+    )
+  }
+
